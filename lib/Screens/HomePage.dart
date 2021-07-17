44eeb818c93d548938168cc1633b980ed6e04af3
Vijay <vijaythevirtue@gmail.com>
2021-07-17 19:14:06 +0530

    refactor

--- lib/Screens/HomePage.dart
+++ lib/Screens/HomePage.dart
@@ -53,52 +53,53 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Widget paymentSuccessful() => Transform.translate(
         offset: Offset(translateX, translateY),
-        child: Align(
-          alignment: Alignment.centerLeft,
-          child: AnimatedContainer(
-            duration: Duration(milliseconds: 200),
-            curve: Curves.linear,
-            width: 100 + myWidth,
-            height: 100,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(50.00),
-              color: Colors.blue,
-            ),
-            child: myWidth > 0.0
-                ? Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.check,
-                        color: Colors.white,
-                        size: 30,
-                      ),
-                      Flexible(
-                        child: Text(
-                          " Payment successful ",
-                          style:
-                              TextStyle(color: Colors.white, fontSize: 20.00),
-                        ),
-                      ),
-                    ],
-                  )
-                : Icon(
-                    Icons.navigate_next,
-                    color: Colors.white,
-                    size: 50.00,
-                  ),
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: 200),
+          curve: Curves.linear,
+          width: 100 + myWidth,
+          height: 100,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50.00),
+            color: Colors.blue,
           ),
+          child: myWidth > 0.0
+              ? Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.check,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+                    Flexible(
+                      child: Text(
+                        " Payment successful ",
+                        style: TextStyle(color: Colors.white, fontSize: 20.00),
+                      ),
+                    ),
+                  ],
+                )
+              : Icon(
+                  Icons.navigate_next,
+                  color: Colors.white,
+                  size: 50.00,
+                ),
         ),
       );
 
   _incTansXVal() async {
-    for (var i = 0; i > -1; i++)
+    int canLoop = -1;
+    for (var i = 0; canLoop == -1; i++)
       await Future.delayed(Duration(milliseconds: 1), () {
         setState(() {
           if (translateX + 1 <
               MediaQuery.of(context).size.width - (200 + myWidth)) {
             translateX += 1;
             myWidth = MediaQuery.of(context).size.width - (200 + myWidth);
+          } else {
+            setState(() {
+              canLoop = 1;
+            });
           }
         });
       });
